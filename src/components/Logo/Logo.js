01090586fedba50597356d8d45b340cff99eead8
Justin Frevert <jgaingringer@gmail.com>
2018-05-20 15:30:23 -0400

    Facebrain init commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './logo.css';
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+             <Tilt className="Tilt br2 bw3 shadow-2 " options={{ max : 45 }} style={{ height: 150, width: 150 }} >
+                  <div className="Tilt-inner pa2 "> <img style={{paddingTop: '5px'}} alt='logo' src={brain} />   </div>
+            </Tilt>
+        </div>
+    );
+}
+export default Logo;
